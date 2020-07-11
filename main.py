810ecaa8b3877f18df37dde257bd2579fbdf62e5
DamianDuy <gamergray8@gmail.com>
2020-07-11 13:36:49 +0200

    Enabled adding positions to the dictionary

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 import json
 from os import system, name
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def clear():
-    if name == 'posix':
-        _ = system('clear')
+    if name == "posix":
+        _ = system("clear")
     else:
-        _ = system('cls')    
+        _ = system("cls")    
 
 def getDef(key):
     if key.lower() in data or key.upper() in data or key.title() in data:
         return data[key]
+    elif len(get_close_matches(key, data.keys(), cutoff = 0.85)) == 1:
+        return "Did you mean %s instead?" % get_close_matches(key, data.keys())[0]
+    elif len(get_close_matches(key, data.keys(), cutoff = 0.85)) == 2:
+        return "Did you mean %s or %s instead?" % (get_close_matches(key,data.keys())[0], get_close_matches(key,data.keys())[1])
+    elif len(get_close_matches(key, data.keys(), cutoff = 0.85)) > 2:
+        return "Did you mean %s, %s or %s instead?" % (get_close_matches(key,data.keys())[0], get_close_matches(key,data.keys())[1], get_close_matches(key,data.keys())[2])   
     else:
-       
-        return "Word does not appear to be in the dictionary."       
+        return "Word does not appear to be in the dictionary."
+
 def addPosition():
-    print("I am not doing anything yet.")
+    key = input("Insert the word: ")
+    value = input("Insert definition: ")
+    data.setdefault(key, [])
+    data[key].append(value)
+    choice = "None"
+    while choice.lower() != "no":
+        choice = input("Do you wish to add another definition of the word? Type yes/no: ")
+        if choice.lower() == "yes":
+            value = input("Insert definition: ")
+            data[key].append(value)
+        elif choice.lower() != "no":
+            print("Type yes/no.")
+
+def addDefin():
+    key = input("Insert the word which definition you wish to add: ")
+    if key in data:
+        value = input("Insert definition: ")
+        data[key].append(value)
+    else:
+        print("The word doesn't appear to be in a dictionary.")
 
 def delPosition():
     print("I am not doing anything yet.")
 
-def show_menu():
+def delDefin():
+    print("I am not doing anything yet.")
+
+def showMenu():
     print("1. Look up the word.")
     print("2. Add a new word.")
     print("3. Add a new definition.")
@@ -31,7 +60,7 @@ def show_menu():
 
 choice = "0"
 while choice != "6":
-    show_menu()
+    showMenu()
     choice = input()
     clear()
     if choice == "1":
@@ -41,10 +70,14 @@ while choice != "6":
         else:
             for defin in getDef(key):
                 print(defin)        
-    if choice == "2" or choice == "3":
+    if choice == "2":
         addPosition()
-    if choice == "4" or choice == "5":
+    if choice == "3":
+        addDefin()    
+    if choice == "4":
         delPosition()
+    if choice == "5":
+        delDefin()    
     if choice != "6":    
         input()            
         clear()
