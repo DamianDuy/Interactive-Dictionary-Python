11d985730cf0c0f788b04fdbb2375a6ed97e05b7
DamianDuy <gamergray8@gmail.com>
2020-07-11 16:49:42 +0200

    Enabled deleting positions from the dictionary and saving dictionary to the file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import json
 from os import system, name
 from difflib import get_close_matches
 
-data = json.load(open("data.json"))
+with open("data.json", "r") as rfile:
+   data = json.load(rfile)
 
 def clear():
     if name == "posix":
@@ -13,11 +14,11 @@ def clear():
 def getDef(key):
     if key.lower() in data or key.upper() in data or key.title() in data:
         return data[key]
-    elif len(get_close_matches(key, data.keys(), cutoff = 0.85)) == 1:
+    elif len(get_close_matches(key, data.keys(), cutoff = 0.8)) == 1:
         return "Did you mean %s instead?" % get_close_matches(key, data.keys())[0]
-    elif len(get_close_matches(key, data.keys(), cutoff = 0.85)) == 2:
+    elif len(get_close_matches(key, data.keys(), cutoff = 0.8)) == 2:
         return "Did you mean %s or %s instead?" % (get_close_matches(key,data.keys())[0], get_close_matches(key,data.keys())[1])
-    elif len(get_close_matches(key, data.keys(), cutoff = 0.85)) > 2:
+    elif len(get_close_matches(key, data.keys(), cutoff = 0.8)) > 2:
         return "Did you mean %s, %s or %s instead?" % (get_close_matches(key,data.keys())[0], get_close_matches(key,data.keys())[1], get_close_matches(key,data.keys())[2])   
     else:
         return "Word does not appear to be in the dictionary."
@@ -45,21 +46,29 @@ def addDefin():
         print("The word doesn't appear to be in a dictionary.")
 
 def delPosition():
-    print("I am not doing anything yet.")
+    key = input("Insert the word you want to delete (coresponding definitions will be deleted as well): ")
+    if key in data:
+        del data[key]
+    else:
+        print("The word doesn't appear to be in a dictionary.")
 
-def delDefin():
-    print("I am not doing anything yet.")
+def SaveDic():
+    with open("data.json", "w") as wfile:
+        json.dump(data, wfile, sort_keys = True, indent = 4)
+    print("Dictionary saved.")    
 
 def showMenu():
     print("1. Look up the word.")
     print("2. Add a new word.")
     print("3. Add a new definition.")
     print("4. Delete the word.")
-    print("5. Delete the definition.")
+    print("5. Save a dictionary.")
     print("6. Exit")
 
 choice = "0"
+ifSaved = False
 while choice != "6":
+    i = 1
     showMenu()
     choice = input()
     clear()
@@ -69,15 +78,23 @@ while choice != "6":
             print(getDef(key))  
         else:
             for defin in getDef(key):
-                print(defin)        
-    if choice == "2":
+                print("{}. {}".format(i, defin))
+                i += 1        
+    elif choice == "2":
         addPosition()
-    if choice == "3":
+    elif choice == "3":
         addDefin()    
-    if choice == "4":
+    elif choice == "4":
         delPosition()
-    if choice == "5":
-        delDefin()    
+    elif choice == "5":
+        SaveDic()
+        ifSaved = True
+    elif choice != "6":
+        print("No such option in the menu.")
+    if choice == "6" and not ifSaved:
+        quitMenu = input("You haven't saved your dictionary. If you are sure you want to quit type 'yes': ")
+        if quitMenu.lower() != "yes":
+            choice = 0                
     if choice != "6":    
         input()            
         clear()
