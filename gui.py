46660415b8a17675dd96022cb7d85368f2d794a5
DamianDuy <gamergray8@gmail.com>
2020-09-21 13:59:47 +0200

    Rewrote gui.py to OOP
    Rewrote the gui.py file to Object Oriented Programming

--- gui.py
+++ gui.py
@@ -1,83 +1,87 @@
 from tkinter import *
 import main
 
-def getDefComm():
-    list1.delete(0, END)
-    i = 1
-    if isinstance(main.getDef(word_text.get()), str):
-            list1.insert(END, main.getDef(word_text.get()))
-    else:
-       for defin in main.getDef(word_text.get()):
-           list1.insert(END, str(i) + ". " + defin)
-           i += 1
-    e1.delete(0, "end")
-    e2.delete(0, "end")
+class Widgets:
+   def __init__(self, window):
+       self.window = window
+       window.title("Dictionary")
 
-def addDefinComm():
-    list1.delete(0, END)
-    list1.insert(END, main.addDefin(word_text.get(), def_text.get()))
-    e1.delete(0, "end")
-    e2.delete(0, END)
+       l1 = Label(window, text = "Word")
+       l1.grid(row = 0, column = 0, sticky = "W", padx = 5, pady = 1)
 
-def addPositionComm():
-     list1.delete(0, END)
-     list1.insert(END, main.addPosition(word_text.get(), def_text.get()))
-     e1.delete(0, "end")
-     e2.delete(0, "end")
+       l2 = Label(window, text = "Definition")
+       l2.grid(row = 1, column = 0, sticky = "W", padx = 5, pady = 1)
 
-def delPositionComm():
-    list1.delete(0, END)
-    list1.insert(END, main.delPosition(word_text.get()))
-    main.delPosition(word_text.get())
-    e1.delete(0, "end")
-    e2.delete(0, "end")
+       self.word_text = StringVar()
+       self.e1 = Entry(window, textvariable = self.word_text, width = 30)
+       self.e1.grid(row = 0, column = 1, sticky = "W")
 
-def saveDicComm():
-    list1.delete(0, END)
-    list1.insert(END, main.saveDic())
+       self.def_text = StringVar()
+       self.e2 = Entry(window, textvariable = self.def_text, width = 30)
+       self.e2.grid(row = 1, column = 1, sticky = "W")
+
+       self.list1 = Listbox(window, height = 10, width = 80)
+       self.list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 2, padx = 5, pady = 5)
+
+       sb1 = Scrollbar(window)
+       sb1.grid(row = 2, column = 2, rowspan = 6)
+
+       self.list1.configure(yscrollcommand = sb1.set)
+       sb1.configure(command = self.list1.yview)
+
+       b1 = Button(window, text = "Look up the word", width = 15, command = self.getDefComm)
+       b1.grid(row = 2, column = 3, padx = 5, pady = 5)
+
+       b2 = Button(window, text = "Add the word", width = 15, command = self.addPositionComm)
+       b2.grid(row = 3, column = 3, padx = 5, pady = 5)
+
+       b3 = Button(window, text = "Add a definition", width = 15, command = self.addDefinComm)
+       b3.grid(row = 4, column = 3, padx = 5, pady = 5)
+
+       b4 = Button(window, text = "Delete the word", width = 15, command = self.delPositionComm)
+       b4.grid(row = 5, column = 3, padx = 5, pady = 5)
+
+       b5 = Button(window, text = "Save your dictionary", width = 15, command = self.saveDicComm)
+       b5.grid(row = 6, column = 3, padx = 5, pady = 5)
+
+       b6 = Button(window, text = "Exit", width = 15, command = window.destroy)
+       b6.grid(row = 7, column = 3, padx = 5, pady = 5)
+
+   def getDefComm(self):
+       self.list1.delete(0, END)
+       i = 1
+       if isinstance(main.getDef(self.word_text.get()), str):
+           self.list1.insert(END, main.getDef(self.word_text.get()))
+       else:
+          for defin in main.getDef(self.word_text.get()):
+              self.list1.insert(END, str(i) + ". " + defin)
+              i += 1
+       self.e1.delete(0, "end")
+       self.e2.delete(0, "end")
+
+   def addDefinComm(self):
+       self.list1.delete(0, END)
+       self.list1.insert(END, main.addDefin(self.word_text.get(), self.def_text.get()))
+       self.e1.delete(0, "end")
+       self.e2.delete(0, END)
+
+   def addPositionComm(self):
+        self.list1.delete(0, END)
+        self.list1.insert(END, main.addPosition(self.word_text.get(), self.def_text.get()))
+        self.e1.delete(0, "end")
+        self.e2.delete(0, "end")
+
+   def delPositionComm(self):
+       self.list1.delete(0, END)
+       self.list1.insert(END, main.delPosition(self.word_text.get()))
+       main.delPosition(self.word_text.get())
+       self.e1.delete(0, "end")
+       self.e2.delete(0, "end")
+
+   def saveDicComm(self):
+       self.list1.delete(0, END)
+       self.list1.insert(END, main.saveDic())
 
 window = Tk()
-window.title("Dictionary")
-
-l1 = Label(window, text = "Word")
-l1.grid(row = 0, column = 0, sticky = "W", padx = 5, pady = 1)
-
-l2 = Label(window, text = "Definition")
-l2.grid(row = 1, column = 0, sticky = "W", padx = 5, pady = 1)
-
-word_text = StringVar()
-e1 = Entry(window, textvariable = word_text, width = 30)
-e1.grid(row = 0, column = 1, sticky = "W")
-
-def_text = StringVar()
-e2 = Entry(window, textvariable = def_text, width = 30)
-e2.grid(row = 1, column = 1, sticky = "W")
-
-list1 = Listbox(window, height = 10, width = 80)
-list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 2, padx = 5, pady = 5)
-
-sb1 = Scrollbar(window)
-sb1.grid(row = 2, column = 2, rowspan = 6)
-
-list1.configure(yscrollcommand = sb1.set)
-sb1.configure(command = list1.yview)
-
-b1 = Button(window, text = "Look up the word", width = 15, command = getDefComm)
-b1.grid(row = 2, column = 3, padx = 5, pady = 5)
-
-b2 = Button(window, text = "Add the word", width = 15, command = addPositionComm)
-b2.grid(row = 3, column = 3, padx = 5, pady = 5)
-
-b3 = Button(window, text = "Add a definition", width = 15, command = addDefinComm)
-b3.grid(row = 4, column = 3, padx = 5, pady = 5)
-
-b4 = Button(window, text = "Delete the word", width = 15, command = delPositionComm)
-b4.grid(row = 5, column = 3, padx = 5, pady = 5)
-
-b5 = Button(window, text = "Save your dictionary", width = 15, command = saveDicComm)
-b5.grid(row = 6, column = 3, padx = 5, pady = 5)
-
-b6 = Button(window, text = "Exit", width = 15, command = window.destroy)
-b6.grid(row = 7, column = 3, padx = 5, pady = 5)
-
+Widgets(window)
 window.mainloop()
